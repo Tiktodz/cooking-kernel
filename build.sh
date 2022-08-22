3ab09545459013a8071bcae9c8f107f51253ee27
T.B.S <ewprjkt@proton.me>
2022-08-23 01:44:19 +0700

    change date

--- build.sh
+++ build.sh
@@ -174,7 +174,7 @@ KERVER=$(make kernelversion)
 COMMIT_HEAD=$(git log --oneline -1)
 
 # Set Date 
-DATE=$(TZ=Asia/Jakarta date +"%Y%m%d")
+DATE=$(TZ=Asia/Jakarta date +"%Y%m%d_%H%M")
 
 #Now Its time for other stuffs like cloning, exporting, etc
 

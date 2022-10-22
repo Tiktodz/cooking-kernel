ea21bdce876caa2cefe646d6cd10208765c9ec59
T.B.S <ewprjkt@proton.me>
2022-10-23 02:59:56 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -48,7 +48,7 @@ KERNEL_DIR="$(pwd)"
 BASEDIR="$(basename "$KERNEL_DIR")"
 
 # The name of the Kernel, to name the ZIP
-ZIPNAME="ElectroWizard-Queue-OCxUV"
+ZIPNAME="ElectroWizard-Queue-SCv1.0"
 
 # Build Author
 # Take care, it should be a universal and most probably, case-sensitive

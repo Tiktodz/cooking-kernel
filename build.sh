a749882148deade12dce85d2e67c43e435583da7
T.B.S <ewprjkt@proton.me>
2022-11-28 20:26:30 +0700

    upVLA

--- build.sh
+++ build.sh
@@ -48,7 +48,7 @@ KERNEL_DIR="$(pwd)"
 BASEDIR="$(basename "$KERNEL_DIR")"
 
 # The name of the Kernel, to name the ZIP
-ZIPNAME="EW-HMP-12LTiramisu"
+ZIPNAME="EW-HMP-VLA-12LTiramisu"
 
 # Build Author
 # Take care, it should be a universal and most probably, case-sensitive

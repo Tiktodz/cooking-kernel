6c40d6aa06b5267c42ec2a99ee81fbd2e4be751e
Meisie Widuri <ewprjkt@proton.me>
2023-07-30 07:30:06 +0700

    eoleas

--- build.sh
+++ build.sh
@@ -51,7 +51,7 @@ BASEDIR="$(basename "$KERNEL_DIR")"
 # Kernel name
 KERNELNAME=TheOneMemory
 CODENAME=Hayzel
-VARIANT=HMP
+VARIANT=EAS
 BASE=EOL
 
 # The name of the Kernel, to name the ZIP
@@ -215,7 +215,7 @@ DATE=$(TZ=Asia/Jakarta date +"%Y%m%d")
   	fi
 
 	msger -n "|| Cloning Anykernel ||"
-	git clone https://github.com/Tiktodz/AnyKernel3.git -b hmp AnyKernel3
+	git clone https://github.com/Tiktodz/AnyKernel3.git -b eas AnyKernel3
 
 	if [ $BUILD_DTBO = 1 ]
 	then
